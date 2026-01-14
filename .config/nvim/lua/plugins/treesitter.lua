2760f84694d8105472a36ef5740f75607c7895d0
Phùng Quốc Việt <92231102+ViegPhunt@users.noreply.github.com>
2026-01-14 23:53:51 +0700

    Update treesitter.lua

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -3,7 +3,7 @@ return {
     build = ":TSUpdate",
 
     config = function()
-        require("nvim-treesitter.configs").setup({
+        require("nvim-treesitter").setup({
             auto_install = true,
             ensure_installed = {
                 "bash",
